b028cfb73b8c86411f693209dcb79f1ebe6549d5
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-03-31 15:00:29 +0200

    [Add] Grafic layout + function random n.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
-console.log(document.title);
+// ===================
+//      GLOBAL
+// ===================
+
+
+
+
+// ===================
+//      EVENTS
+// ===================
+
+
+
+
+// ===================
+//      FUNCTION
+// ===================
+
+function generateRandomNumbers(max) { //Max sarà determinato dal livello.
+
+  const randomNumber = Math.floor(Math.random()* max - 1 + 1) + 1
+
+  return randomNumber
+}
