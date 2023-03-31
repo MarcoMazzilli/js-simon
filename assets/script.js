66ca838696c2d2812f8a4d7b231199282d1e3181
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-03-31 18:39:43 +0200

    [Add] Outupt message

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,18 @@
 // ===================
 const btnStart          = document.getElementById("btnStart");
 const OUTPUT            = document.getElementById("numbersContainerOutput");
-const numeriInseriti    = [];
+const numeriIndovinati  = [];
+
+
 // ===================
 //      EVENTS
 // ===================
 
 
 btnStart.addEventListener("click", function(){
+
+  numeriInseriti = [];
+  OUTPUT.innerHTML = "";
   
   const livello = document.querySelector("select").value
 
@@ -24,35 +29,38 @@ btnStart.addEventListener("click", function(){
 
   setTimeout( function(){
     OUTPUT.innerHTML = [];
-
+      numeriInseriti = [];
     while (numeriInseriti.length < numeriGenerati.length) {
       const numeroInserito = prompt("Inserisci un numero")
 
       if (isNaN(numeroInserito)) {
-       console.warn("non e un numero")
+       console.warn("is not a number")
       }else{
-         numeriInseriti.push(numeroInserito)
-         console.log("questo e un numero")
+         numeriInseriti.push(parseInt(numeroInserito))
+         console.log("yes is a number")
+         console.log(numeriInseriti)
       }
    }
-
-
 
 
     let counter = 0;
   
    for (let i = 0; i < numeriGenerati.length; i++) {
      if (!numeriGenerati.includes(numeriInseriti[i])){
-      continue
-     }else{
-       counter++
+       console.warn("non lo continene")
+      }else{
+        counter++
+        numeriIndovinati.push(numeriInseriti[i])
+        console.log("lo contiene")
       }
       
     }
+
     console.log("numeriInseriti",numeriInseriti)
     console.log('punteggio',counter)
-  },5000)
 
+    OUTPUT.innerHTML  = `<h3 class="fw-bolder">Hai totalizzato ${counter} punti e hai indovinato i numeri ${numeriIndovinati}</h3>`;
+  },5000)
 
 
 })
@@ -68,7 +76,7 @@ function generateRandomNumbers(livello) {     //Max sarà determinato dal livell
   
   while (numeri.length < livello) {
     
-    const numero = Math.floor(Math.random()* 100 - 1 + 1) + 1;
+    const numero = Math.floor(Math.random()* 50 - 1 + 1) + 1;
 
     if (!numeri.includes(numero)) numeri.push(numero);
 
